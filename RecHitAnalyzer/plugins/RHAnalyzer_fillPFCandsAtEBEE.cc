adea5314030c96c722ac810f02543f006f6e76b8
johnalison <johnalison@cmu.edu>
2019-03-23 18:00:29 -0500

    Update with PV reqs

--- RecHitAnalyzer/plugins/RHAnalyzer_fillPFCandsAtEBEE.cc
+++ RecHitAnalyzer/plugins/RHAnalyzer_fillPFCandsAtEBEE.cc
@@ -6,12 +6,26 @@
 
 std::vector<float> vEndTracksPt_EE_[nEE];
 std::vector<float> vEndTracksQPt_EE_[nEE];
+std::vector<float> vEndTracksPt_EE_PV_[nEE];
+std::vector<float> vEndTracksQPt_EE_PV_[nEE];
+std::vector<float> vEndTracksPt_EE_nPV_[nEE];
+std::vector<float> vEndTracksQPt_EE_nPV_[nEE];
+
 std::vector<float> vMuonsPt_EE_[nEE];
 std::vector<float> vMuonsQPt_EE_[nEE];
+std::vector<float> vMuonsPt_EE_PV_[nEE];
+std::vector<float> vMuonsQPt_EE_PV_[nEE];
+
 std::vector<float> vEndTracksPt_EB_;
 std::vector<float> vEndTracksQPt_EB_;
+std::vector<float> vEndTracksPt_EB_PV_;
+std::vector<float> vEndTracksQPt_EB_PV_;
+std::vector<float> vEndTracksPt_EB_nPV_;
+std::vector<float> vEndTracksQPt_EB_nPV_;
 std::vector<float> vMuonsPt_EB_;
 std::vector<float> vMuonsQPt_EB_;
+std::vector<float> vMuonsPt_EB_PV_;
+std::vector<float> vMuonsQPt_EB_PV_;
 
 // Initialize branches ____________________________________________________________//
 void RecHitAnalyzer::branchesPFCandsAtEBEE ( TTree* tree, edm::Service<TFileService> &fs ) {
@@ -19,21 +33,31 @@ void RecHitAnalyzer::branchesPFCandsAtEBEE ( TTree* tree, edm::Service<TFileServ
   // Branches for images
   tree->Branch("EndTracksPt_EB",  &vEndTracksPt_EB_);
   tree->Branch("EndTracksQPt_EB", &vEndTracksQPt_EB_);
+  tree->Branch("EndTracksPt_EB_PV",  &vEndTracksPt_EB_PV_);
+  tree->Branch("EndTracksQPt_EB_PV", &vEndTracksQPt_EB_PV_);
+  tree->Branch("EndTracksPt_EB_nPV",  &vEndTracksPt_EB_nPV_);
+  tree->Branch("EndTracksQPt_EB_npPV", &vEndTracksQPt_EB_nPV_);
   tree->Branch("MuonsPt_EB",  &vMuonsPt_EB_);
   tree->Branch("MuonsQPt_EB", &vMuonsQPt_EB_);
+  tree->Branch("MuonsPt_EB_PV",  &vMuonsPt_EB_PV_);
+  tree->Branch("MuonsQPt_EB_PV", &vMuonsQPt_EB_PV_);
 
   char hname[50];
   for ( int iz(0); iz < nEE; iz++ ) {
     // Branches for images
     const char *zside = (iz > 0) ? "p" : "m";
-    sprintf(hname, "EndTracksPt_EE%s",zside);
-    tree->Branch(hname,        &vEndTracksPt_EE_[iz]);
-    sprintf(hname, "EndTracksQPt_EE%s",zside);
-    tree->Branch(hname,        &vEndTracksQPt_EE_[iz]);
-    sprintf(hname, "MuonsPt_EE%s",zside);
-    tree->Branch(hname,        &vMuonsPt_EE_[iz]);
-    sprintf(hname, "MuonsQPt_EE%s",zside);
-    tree->Branch(hname,        &vMuonsQPt_EE_[iz]);
+    sprintf(hname, "EndTracksPt_EE%s",zside);      tree->Branch(hname,        &vEndTracksPt_EE_[iz]);
+    sprintf(hname, "EndTracksQPt_EE%s",zside);     tree->Branch(hname,        &vEndTracksQPt_EE_[iz]);
+    sprintf(hname, "EndTracksPt_PV_EE%s",zside);   tree->Branch(hname,        &vEndTracksPt_EE_PV_[iz]);
+    sprintf(hname, "EndTracksQPt_PV_EE%s",zside);  tree->Branch(hname,        &vEndTracksQPt_EE_PV_[iz]);
+    sprintf(hname, "EndTracksPt_nPV_EE%s",zside);  tree->Branch(hname,        &vEndTracksPt_EE_nPV_[iz]);
+    sprintf(hname, "EndTracksQPt_nPV_EE%s",zside); tree->Branch(hname,        &vEndTracksQPt_EE_nPV_[iz]);
+
+    sprintf(hname, "MuonsPt_EE%s",zside);          tree->Branch(hname,        &vMuonsPt_EE_[iz]);
+    sprintf(hname, "MuonsQPt_EE%s",zside);         tree->Branch(hname,        &vMuonsQPt_EE_[iz]);
+    sprintf(hname, "MuonsPt_PV_EE%s",zside);       tree->Branch(hname,        &vMuonsPt_EE_PV_[iz]);
+    sprintf(hname, "MuonsQPt_PV_EE%s",zside);      tree->Branch(hname,        &vMuonsQPt_EE_PV_[iz]);
+    
   } // iz
 
 } // branchesEB()
@@ -48,13 +72,25 @@ void RecHitAnalyzer::fillPFCandsAtEBEE ( const edm::Event& iEvent, const edm::Ev
 
   vEndTracksPt_EB_.assign( EBDetId::kSizeForDenseIndexing, 0. );
   vEndTracksQPt_EB_.assign( EBDetId::kSizeForDenseIndexing, 0. );
+  vEndTracksPt_EB_PV_.assign( EBDetId::kSizeForDenseIndexing, 0. );
+  vEndTracksQPt_EB_PV_.assign( EBDetId::kSizeForDenseIndexing, 0. );
+  vEndTracksPt_EB_nPV_.assign( EBDetId::kSizeForDenseIndexing, 0. );
+  vEndTracksQPt_EB_nPV_.assign( EBDetId::kSizeForDenseIndexing, 0. );
   vMuonsPt_EB_.assign( EBDetId::kSizeForDenseIndexing, 0. );
   vMuonsQPt_EB_.assign( EBDetId::kSizeForDenseIndexing, 0. );
+  vMuonsPt_EB_PV_.assign( EBDetId::kSizeForDenseIndexing, 0. );
+  vMuonsQPt_EB_PV_.assign( EBDetId::kSizeForDenseIndexing, 0. );
   for ( int iz(0); iz < nEE; iz++ ) {
     vEndTracksPt_EE_[iz].assign( EE_NC_PER_ZSIDE, 0. );
     vEndTracksQPt_EE_[iz].assign( EE_NC_PER_ZSIDE, 0. );
+    vEndTracksPt_EE_PV_[iz].assign( EE_NC_PER_ZSIDE, 0. );
+    vEndTracksQPt_EE_PV_[iz].assign( EE_NC_PER_ZSIDE, 0. );
+    vEndTracksPt_EE_nPV_[iz].assign( EE_NC_PER_ZSIDE, 0. );
+    vEndTracksQPt_EE_nPV_[iz].assign( EE_NC_PER_ZSIDE, 0. );
     vMuonsPt_EE_[iz].assign( EE_NC_PER_ZSIDE, 0. );
     vMuonsQPt_EE_[iz].assign( EE_NC_PER_ZSIDE, 0. );
+    vMuonsPt_EE_PV_[iz].assign( EE_NC_PER_ZSIDE, 0. );
+    vMuonsQPt_EE_PV_[iz].assign( EE_NC_PER_ZSIDE, 0. );
   }
 
   edm::Handle<PFCollection> pfCandsH_;
@@ -65,6 +101,10 @@ void RecHitAnalyzer::fillPFCandsAtEBEE ( const edm::Event& iEvent, const edm::Ev
   iSetup.get<CaloGeometryRecord>().get( caloGeomH_ );
   const CaloGeometry* caloGeom = caloGeomH_.product();
 
+  edm::Handle<reco::VertexCollection> vertexInfo;
+  iEvent.getByToken(vertexCollectionT_, vertexInfo);
+  const reco::VertexCollection& vtxs = *vertexInfo;
+
 
   for ( PFCollection::const_iterator iPFC = pfCandsH_->begin();
         iPFC != pfCandsH_->end(); ++iPFC ) {
@@ -74,21 +114,43 @@ void RecHitAnalyzer::fillPFCandsAtEBEE ( const edm::Event& iEvent, const edm::Ev
     const math::XYZPointF& ecalPos = iPFC->positionAtECALEntrance();
     eta = ecalPos.eta();
     phi = ecalPos.phi();
+    float z0    =  ( !vtxs.empty() ? thisTrk->dz(vtxs[0].position())  : thisTrk->dz() );
     
     if ( std::abs(eta) > 3. ) continue;
     DetId id( spr::findDetIdECAL( caloGeom, eta, phi, false ) );
+
+    float thisTrkPt = thisTrk->pt();
+    float thisTrkQPt = (thisTrk->pt()*thisTrk->charge());
+    
     if ( id.subdetId() == EcalBarrel ) {
       EBDetId ebId( id );
 
       idx_ = ebId.hashedIndex(); // (ieta_+EB_IETA_MAX)*EB_IPHI_MAX + iphi_
       // Fill vectors for images
       
-      vEndTracksPt_EB_[idx_] += thisTrk->pt();
-      vEndTracksQPt_EB_[idx_] += (thisTrk->pt()*thisTrk->charge());
+      vEndTracksPt_EB_[idx_] += thisTrkPt;
+      vEndTracksQPt_EB_[idx_] += thisTrkQPt;
       if(iPFC->particleId() == 3){
-	vMuonsPt_EB_[idx_] += thisTrk->pt();
-	vMuonsPt_EB_[idx_] += (thisTrk->pt()*thisTrk->charge());
+	vMuonsPt_EB_[idx_] +=thisTrkPt;
+	vMuonsPt_EB_[idx_] += thisTrkQPt;
       }
+
+      if(fabs(z0) < z0PVCut_){
+	vEndTracksPt_EB_PV_[idx_] += thisTrkPt;
+	vEndTracksQPt_EB_PV_[idx_] += thisTrkQPt;
+
+	if(iPFC->particleId() == 3){
+	  vMuonsPt_EB_PV_[idx_] += thisTrkPt;
+	  vMuonsPt_EB_PV_[idx_] += thisTrkQPt;
+	}
+	
+      }else{
+	vEndTracksPt_EB_nPV_[idx_] += thisTrkPt;
+	vEndTracksQPt_EB_nPV_[idx_] += thisTrkQPt;
+      }
+
+
+
     } else if ( id.subdetId() == EcalEndcap ) {
       EEDetId eeId( id );
       ix_ = eeId.ix() - 1;
@@ -98,12 +160,27 @@ void RecHitAnalyzer::fillPFCandsAtEBEE ( const edm::Event& iEvent, const edm::Ev
       // Create hashed Index: maps from [iy][ix] -> [idx_]
       idx_ = iy_*EE_MAX_IX + ix_;
       // Fill vectors for images
-      vEndTracksPt_EE_[iz_][idx_] += thisTrk->pt();
-      vEndTracksQPt_EE_[iz_][idx_] += (thisTrk->pt()*thisTrk->charge());
+      vEndTracksPt_EE_[iz_][idx_] += thisTrkPt;
+      vEndTracksQPt_EE_[iz_][idx_] += thisTrkQPt;
       if(iPFC->particleId() == 3){
-	vMuonsPt_EE_[iz_][idx_] += thisTrk->pt();
-	vMuonsQPt_EE_[iz_][idx_] += (thisTrk->pt()*thisTrk->charge());
+	vMuonsPt_EE_[iz_][idx_] += thisTrkPt;
+	vMuonsQPt_EE_[iz_][idx_] += thisTrkQPt;
       }
+
+
+      if(fabs(z0) < z0PVCut_){
+	vEndTracksPt_EE_PV_[iz_][idx_] += thisTrkPt;
+	vEndTracksQPt_EE_PV_[iz_][idx_] += thisTrkQPt;
+	if(iPFC->particleId() == 3){
+	  vMuonsPt_EE_PV_[iz_][idx_] += thisTrkPt;
+	  vMuonsQPt_EE_PV_[iz_][idx_] += thisTrkQPt;
+	}
+
+      }else{
+	vEndTracksPt_EE_nPV_[iz_][idx_] += thisTrkPt;
+	vEndTracksQPt_EE_nPV_[iz_][idx_] += thisTrkQPt;
+      }
+
     } 
   }//PF Candidates
 
--- RecHitAnalyzer/plugins/RHAnalyzer_fillPFCandsAtECALstitched.cc
+++ RecHitAnalyzer/plugins/RHAnalyzer_fillPFCandsAtECALstitched.cc
@@ -6,14 +6,27 @@
 
 TH2F *hEvt_EE_EndtracksPt[nEE];
 TH2F *hEvt_EE_EndtracksQPt[nEE];
+TH2F *hEvt_EE_EndtracksPt_PV[nEE];
+TH2F *hEvt_EE_EndtracksQPt_PV[nEE];
+TH2F *hEvt_EE_EndtracksPt_nPV[nEE];
+TH2F *hEvt_EE_EndtracksQPt_nPV[nEE];
 TH2F *hEvt_EE_muonsPt[nEE];
 TH2F *hEvt_EE_muonsQPt[nEE];
+TH2F *hEvt_EE_muonsPt_PV[nEE];
+TH2F *hEvt_EE_muonsQPt_PV[nEE];
+
 TProfile2D *hECAL_EndtracksPt;
 TProfile2D *hECAL_muonsPt;
 std::vector<float> vECAL_EndtracksPt_;
 std::vector<float> vECAL_EndtracksQPt_;
+std::vector<float> vECAL_EndtracksPt_PV_;
+std::vector<float> vECAL_EndtracksQPt_PV_;
+std::vector<float> vECAL_EndtracksPt_nPV_;
+std::vector<float> vECAL_EndtracksQPt_nPV_;
 std::vector<float> vECAL_muonsPt_;
 std::vector<float> vECAL_muonsQPt_;
+std::vector<float> vECAL_muonsPt_PV_;
+std::vector<float> vECAL_muonsQPt_PV_;
 
 // Initialize branches _______________________________________________________________//
 void RecHitAnalyzer::branchesPFCandsAtECALstitched ( TTree* tree, edm::Service<TFileService> &fs ) {
@@ -21,39 +34,69 @@ void RecHitAnalyzer::branchesPFCandsAtECALstitched ( TTree* tree, edm::Service<T
   // Branches for images
   tree->Branch("ECAL_EndtracksPt",    &vECAL_EndtracksPt_);
   tree->Branch("ECAL_EndtracksQPt",   &vECAL_EndtracksQPt_);
+
+  tree->Branch("ECAL_EndtracksPt_PV",    &vECAL_EndtracksPt_PV_);
+  tree->Branch("ECAL_EndtracksQPt_PV",   &vECAL_EndtracksQPt_PV_);
+
+  tree->Branch("ECAL_EndtracksPt_nPV",    &vECAL_EndtracksPt_nPV_);
+  tree->Branch("ECAL_EndtracksQPt_nPV",   &vECAL_EndtracksQPt_nPV_);
+
   tree->Branch("ECAL_muonsPt",        &vECAL_muonsPt_);
   tree->Branch("ECAL_muonsQPt",       &vECAL_muonsQPt_);
 
+  tree->Branch("ECAL_muonsPt_PV",        &vECAL_muonsPt_PV_);
+  tree->Branch("ECAL_muonsQPt_PV",       &vECAL_muonsQPt_PV_);
+
+  static const std::string strIndex[2] = {"m","p"};
+  static const double* binIndex[2] = {eta_bins_EEm, eta_bins_EEp};
+
+
   // Intermediate helper histogram (single event only)
-  hEvt_EE_EndtracksPt[0] = new TH2F("evt_EEm_EndtracksPt", "E(i#phi,i#eta);i#phi;i#eta",
+  for(unsigned int idx = 0; idx < 2; ++idx){
+    hEvt_EE_EndtracksPt[idx] = new TH2F(("evt_EE"+strIndex[idx]+"_EndtracksPt").c_str(), "E(i#phi,i#eta);i#phi;i#eta",
+					EB_IPHI_MAX, -TMath::Pi(), TMath::Pi(),
+					5*(HBHE_IETA_MAX_HE-1-HBHE_IETA_MAX_EB), binIndex[idx] );
+
+    hEvt_EE_EndtracksQPt[idx] = new TH2F(("evt_EE"+strIndex[idx]+"_EndtracksQPt").c_str(), "QPt(i#phi,i#eta);i#phi;i#eta",
+					 EB_IPHI_MAX, -TMath::Pi(), TMath::Pi(),
+					 5*(HBHE_IETA_MAX_HE-1-HBHE_IETA_MAX_EB), binIndex[idx] );
+
+
+    hEvt_EE_EndtracksPt_PV[idx] = new TH2F(("evt_EE"+strIndex[idx]+"_EndtracksPt_PV").c_str(), "E(i#phi,i#eta);i#phi;i#eta",
+					   EB_IPHI_MAX, -TMath::Pi(), TMath::Pi(),
+					   5*(HBHE_IETA_MAX_HE-1-HBHE_IETA_MAX_EB), binIndex[idx] );
+
+    hEvt_EE_EndtracksQPt_PV[idx] = new TH2F(("evt_EE"+strIndex[idx]+"_EndtracksQPt_PV").c_str(), "QPt(i#phi,i#eta);i#phi;i#eta",
+					    EB_IPHI_MAX, -TMath::Pi(), TMath::Pi(),
+					    5*(HBHE_IETA_MAX_HE-1-HBHE_IETA_MAX_EB), binIndex[idx] );
+
+    hEvt_EE_EndtracksPt_nPV[idx] = new TH2F(("evt_EE"+strIndex[idx]+"_EndtracksPt_nPV").c_str(), "E(i#phi,i#eta);i#phi;i#eta",
+					    EB_IPHI_MAX, -TMath::Pi(), TMath::Pi(),
+					    5*(HBHE_IETA_MAX_HE-1-HBHE_IETA_MAX_EB), binIndex[idx] );
+
+    hEvt_EE_EndtracksQPt_nPV[idx] = new TH2F(("evt_EE"+strIndex[idx]+"_EndtracksQPt_nPV").c_str(), "QPt(i#phi,i#eta);i#phi;i#eta",
+					     EB_IPHI_MAX, -TMath::Pi(), TMath::Pi(),
+					     5*(HBHE_IETA_MAX_HE-1-HBHE_IETA_MAX_EB), binIndex[idx] );
+
+
+
+    hEvt_EE_muonsPt[idx] = new TH2F(("evt_EE"+strIndex[idx]+"_muonsPt").c_str(), "E(i#phi,i#eta);i#phi;i#eta",
 				    EB_IPHI_MAX, -TMath::Pi(), TMath::Pi(),
-				    5*(HBHE_IETA_MAX_HE-1-HBHE_IETA_MAX_EB), eta_bins_EEm );
-  hEvt_EE_EndtracksPt[1] = new TH2F("evt_EEp_EndtracksPt", "E(i#phi,i#eta);i#phi;i#eta",
-				    EB_IPHI_MAX, -TMath::Pi(), TMath::Pi(),
-				    5*(HBHE_IETA_MAX_HE-1-HBHE_IETA_MAX_EB), eta_bins_EEp );
+				    5*(HBHE_IETA_MAX_HE-1-HBHE_IETA_MAX_EB), binIndex[idx] );
 
-  hEvt_EE_EndtracksQPt[0] = new TH2F("evt_EEm_EndtracksQPt", "QPt(i#phi,i#eta);i#phi;i#eta",
+    hEvt_EE_muonsQPt[idx] = new TH2F(("evt_EE"+strIndex[idx]+"_muonsQPt").c_str(), "QPt(i#phi,i#eta);i#phi;i#eta",
 				     EB_IPHI_MAX, -TMath::Pi(), TMath::Pi(),
-				     5*(HBHE_IETA_MAX_HE-1-HBHE_IETA_MAX_EB), eta_bins_EEm );
-  hEvt_EE_EndtracksQPt[1] = new TH2F("evt_EEp_EndtracksQPt", "QPt(i#phi,i#eta);i#phi;i#eta",
-				     EB_IPHI_MAX, -TMath::Pi(), TMath::Pi(),
-				     5*(HBHE_IETA_MAX_HE-1-HBHE_IETA_MAX_EB), eta_bins_EEp );
+				     5*(HBHE_IETA_MAX_HE-1-HBHE_IETA_MAX_EB), binIndex[idx] );
 
+    hEvt_EE_muonsPt_PV[idx] = new TH2F(("evt_EE"+strIndex[idx]+"_muonsPt_PV").c_str(), "E(i#phi,i#eta);i#phi;i#eta",
+				       EB_IPHI_MAX, -TMath::Pi(), TMath::Pi(),
+				       5*(HBHE_IETA_MAX_HE-1-HBHE_IETA_MAX_EB), binIndex[idx] );
 
-  hEvt_EE_muonsPt[0] = new TH2F("evt_EEm_muonsPt", "E(i#phi,i#eta);i#phi;i#eta",
-				EB_IPHI_MAX, -TMath::Pi(), TMath::Pi(),
-				5*(HBHE_IETA_MAX_HE-1-HBHE_IETA_MAX_EB), eta_bins_EEm );
-  hEvt_EE_muonsPt[1] = new TH2F("evt_EEp_muonsPt", "E(i#phi,i#eta);i#phi;i#eta",
-				EB_IPHI_MAX, -TMath::Pi(), TMath::Pi(),
-				5*(HBHE_IETA_MAX_HE-1-HBHE_IETA_MAX_EB), eta_bins_EEp );
+    hEvt_EE_muonsQPt_PV[idx] = new TH2F(("evt_EE"+strIndex[idx]+"_muonsQPt_PV").c_str(), "QPt(i#phi,i#eta);i#phi;i#eta",
+					EB_IPHI_MAX, -TMath::Pi(), TMath::Pi(),
+					5*(HBHE_IETA_MAX_HE-1-HBHE_IETA_MAX_EB), binIndex[idx] );
 
-  hEvt_EE_muonsQPt[0] = new TH2F("evt_EEm_muonsQPt", "QPt(i#phi,i#eta);i#phi;i#eta",
-				 EB_IPHI_MAX, -TMath::Pi(), TMath::Pi(),
-				 5*(HBHE_IETA_MAX_HE-1-HBHE_IETA_MAX_EB), eta_bins_EEm );
-  hEvt_EE_muonsQPt[1] = new TH2F("evt_EEp_muonsQPt", "QPt(i#phi,i#eta);i#phi;i#eta",
-				 EB_IPHI_MAX, -TMath::Pi(), TMath::Pi(),
-				 5*(HBHE_IETA_MAX_HE-1-HBHE_IETA_MAX_EB), eta_bins_EEp );
-
+  }
 
 
   // Histograms for monitoring
@@ -68,12 +111,17 @@ void RecHitAnalyzer::branchesPFCandsAtECALstitched ( TTree* tree, edm::Service<T
 } // branchesTracksAtECALstitched()
 
 // Function to map EE(phi,eta) histograms to ECAL(iphi,ieta) vector _______________________________//
-void fillPFCandsAtECAL_with_EEproj ( TH2F *hEvt_EE_EndtracksPt_, TH2F *hEvt_EE_EndtracksQPt_, TH2F *hEvt_EE_muonsPt_, TH2F *hEvt_EE_muonsQPt_, int ieta_global_offset, int ieta_signed_offset ) {
+void fillPFCandsAtECAL_with_EEproj ( TH2F *hEvt_EE_EndtracksPt_, TH2F *hEvt_EE_EndtracksQPt_, 
+				     TH2F *hEvt_EE_EndtracksPt_PV_, TH2F *hEvt_EE_EndtracksQPt_PV_, 
+				     TH2F *hEvt_EE_EndtracksPt_nPV_, TH2F *hEvt_EE_EndtracksQPt_nPV_, 
+				     TH2F *hEvt_EE_muonsPt_, TH2F *hEvt_EE_muonsQPt_, 
+				     TH2F *hEvt_EE_muonsPt_PV, TH2F *hEvt_EE_muonsQPt_PV, 
+				     int ieta_global_offset, int ieta_signed_offset ) {
 
   int ieta_global_, ieta_signed_;
   int ieta_, iphi_, idx_;
-  float EndtrackPt_, EndtrackQPt_;
-  float muonPt_, muonQPt_;
+  float EndtrackPt_, EndtrackQPt_, EndtrackPt_PV_, EndtrackQPt_PV_, EndtrackPt_nPV_, EndtrackQPt_nPV_;
+  float muonPt_, muonQPt_,muonPt_PV_, muonQPt_PV_;
 
   for (int ieta = 1; ieta < hEvt_EE_EndtracksPt_->GetNbinsY()+1; ieta++) {
     ieta_ = ieta - 1;
@@ -83,8 +131,14 @@ void fillPFCandsAtECAL_with_EEproj ( TH2F *hEvt_EE_EndtracksPt_, TH2F *hEvt_EE_E
 
       EndtrackPt_  = hEvt_EE_EndtracksPt_->GetBinContent( iphi, ieta );
       EndtrackQPt_ = hEvt_EE_EndtracksQPt_->GetBinContent( iphi, ieta );
+      EndtrackPt_PV_  = hEvt_EE_EndtracksPt_PV_->GetBinContent( iphi, ieta );
+      EndtrackQPt_PV_ = hEvt_EE_EndtracksQPt_PV_->GetBinContent( iphi, ieta );
+      EndtrackPt_nPV_  = hEvt_EE_EndtracksPt_nPV_->GetBinContent( iphi, ieta );
+      EndtrackQPt_nPV_ = hEvt_EE_EndtracksQPt_nPV_->GetBinContent( iphi, ieta );
       muonPt_ = hEvt_EE_muonsPt_->GetBinContent( iphi, ieta );
       muonQPt_ = hEvt_EE_muonsQPt_->GetBinContent( iphi, ieta );
+      muonPt_PV_ = hEvt_EE_muonsPt_PV->GetBinContent( iphi, ieta );
+      muonQPt_PV_ = hEvt_EE_muonsQPt_PV->GetBinContent( iphi, ieta );
       if ( (EndtrackPt_ <= zs)  && (muonPt_ <= zs)) continue;
       // NOTE: EB iphi = 1 does not correspond to physical phi = -pi so need to shift!
       iphi_ = iphi  + 5*38; // shift
@@ -94,8 +148,14 @@ void fillPFCandsAtECAL_with_EEproj ( TH2F *hEvt_EE_EndtracksPt_, TH2F *hEvt_EE_E
       // Fill vector for image
       vECAL_EndtracksPt_[idx_]  = EndtrackPt_;
       vECAL_EndtracksQPt_[idx_] = EndtrackQPt_;
+      vECAL_EndtracksPt_PV_[idx_]  = EndtrackPt_PV_;
+      vECAL_EndtracksQPt_PV_[idx_] = EndtrackQPt_PV_;
+      vECAL_EndtracksPt_nPV_[idx_]  = EndtrackPt_nPV_;
+      vECAL_EndtracksQPt_nPV_[idx_] = EndtrackQPt_nPV_;
       vECAL_muonsPt_[idx_] = muonPt_;
       vECAL_muonsQPt_[idx_] = muonQPt_;
+      vECAL_muonsPt_PV_[idx_] = muonPt_PV_;
+      vECAL_muonsQPt_PV_[idx_] = muonQPt_PV_;
       // Fill histogram for monitoring
       hECAL_EndtracksPt->Fill( iphi_, ieta_signed_, EndtrackPt_ );
       hECAL_muonsPt->Fill( iphi_, ieta_signed_, muonPt_ );
@@ -116,12 +176,24 @@ void RecHitAnalyzer::fillPFCandsAtECALstitched ( const edm::Event& iEvent, const
 
   vECAL_EndtracksPt_.assign( 2*ECAL_IETA_MAX_EXT*EB_IPHI_MAX, 0. );
   vECAL_EndtracksQPt_.assign( 2*ECAL_IETA_MAX_EXT*EB_IPHI_MAX, 0. );
+  vECAL_EndtracksPt_PV_.assign( 2*ECAL_IETA_MAX_EXT*EB_IPHI_MAX, 0. );
+  vECAL_EndtracksQPt_PV_.assign( 2*ECAL_IETA_MAX_EXT*EB_IPHI_MAX, 0. );
+  vECAL_EndtracksPt_nPV_.assign( 2*ECAL_IETA_MAX_EXT*EB_IPHI_MAX, 0. );
+  vECAL_EndtracksQPt_nPV_.assign( 2*ECAL_IETA_MAX_EXT*EB_IPHI_MAX, 0. );
   vECAL_muonsPt_.assign( 2*ECAL_IETA_MAX_EXT*EB_IPHI_MAX, 0. );
   vECAL_muonsQPt_.assign( 2*ECAL_IETA_MAX_EXT*EB_IPHI_MAX, 0. );
-  for ( int iz(0); iz < nEE; ++iz ) hEvt_EE_EndtracksPt[iz]->Reset();
-  for ( int iz(0); iz < nEE; ++iz ) hEvt_EE_EndtracksQPt[iz]->Reset();
-  for ( int iz(0); iz < nEE; ++iz ) hEvt_EE_muonsPt[iz]->Reset();
-  for ( int iz(0); iz < nEE; ++iz ) hEvt_EE_muonsQPt[iz]->Reset();
+  vECAL_muonsPt_PV_.assign( 2*ECAL_IETA_MAX_EXT*EB_IPHI_MAX, 0. );
+  vECAL_muonsQPt_PV_.assign( 2*ECAL_IETA_MAX_EXT*EB_IPHI_MAX, 0. );
+  for ( int iz(0); iz < nEE; ++iz ) {
+    hEvt_EE_EndtracksPt[iz]->Reset();
+    hEvt_EE_EndtracksQPt[iz]->Reset();
+    hEvt_EE_EndtracksPt_PV[iz]->Reset();
+    hEvt_EE_EndtracksQPt_PV[iz]->Reset();
+    hEvt_EE_EndtracksPt_nPV[iz]->Reset();
+    hEvt_EE_EndtracksQPt_nPV[iz]->Reset();
+    hEvt_EE_muonsPt[iz]->Reset();
+    hEvt_EE_muonsQPt[iz]->Reset();
+  }
 
   edm::Handle<EcalRecHitCollection> EBRecHitsH_;
   iEvent.getByToken( EBRecHitCollectionT_, EBRecHitsH_ );
@@ -131,9 +203,13 @@ void RecHitAnalyzer::fillPFCandsAtECALstitched ( const edm::Event& iEvent, const
   iSetup.get<CaloGeometryRecord>().get( caloGeomH_ );
   const CaloGeometry* caloGeom = caloGeomH_.product();
 
-
   edm::Handle<PFCollection> pfCandsH_;
   iEvent.getByToken( pfCollectionT_, pfCandsH_ );
+
+  edm::Handle<reco::VertexCollection> vertexInfo;
+  iEvent.getByToken(vertexCollectionT_, vertexInfo);
+  const reco::VertexCollection& vtxs = *vertexInfo;
+
 
   for ( PFCollection::const_iterator iPFC = pfCandsH_->begin();
         iPFC != pfCandsH_->end(); ++iPFC ) {
@@ -149,13 +225,36 @@ void RecHitAnalyzer::fillPFCandsAtECALstitched ( const edm::Event& iEvent, const
     if ( id.subdetId() == EcalBarrel ) continue;
     if ( id.subdetId() == EcalEndcap ) {
       iz_ = (eta > 0.) ? 1 : 0;
+
       // Fill intermediate helper histogram by eta,phi
       hEvt_EE_EndtracksPt[iz_]->Fill( phi, eta, thisTrk->pt() );
       hEvt_EE_EndtracksQPt[iz_]->Fill( phi, eta, thisTrk->charge() * thisTrk->pt() );
+
       if(iPFC->particleId() == 3){
 	hEvt_EE_muonsPt[iz_]->Fill( phi, eta, thisTrk->pt() );
 	hEvt_EE_muonsQPt[iz_]->Fill( phi, eta, thisTrk->charge()*thisTrk->pt() );
       }
+
+      const double z0 = ( !vtxs.empty() ? thisTrk->dz(vtxs[0].position()) : thisTrk->dz() );
+
+      // if is PV
+      if(fabs(z0) < z0PVCut_){
+
+	hEvt_EE_EndtracksPt_PV[iz_]->Fill( phi, eta, thisTrk->pt() );
+	hEvt_EE_EndtracksQPt_PV[iz_]->Fill( phi, eta, thisTrk->charge() * thisTrk->pt() );
+
+	if(iPFC->particleId() == 3){
+	  hEvt_EE_muonsPt_PV[iz_]->Fill( phi, eta, thisTrk->pt() );
+	  hEvt_EE_muonsQPt_PV[iz_]->Fill( phi, eta, thisTrk->charge()*thisTrk->pt() );
+	}
+
+      }else{	// if is not PV
+	
+	hEvt_EE_EndtracksPt_nPV[iz_]->Fill( phi, eta, thisTrk->pt() );
+	hEvt_EE_EndtracksQPt_nPV[iz_]->Fill( phi, eta, thisTrk->charge() * thisTrk->pt() );
+      }
+
+
     }
   } // pfCands
 
@@ -164,7 +263,12 @@ void RecHitAnalyzer::fillPFCandsAtECALstitched ( const edm::Event& iEvent, const
   // Map EE-(phi,eta) to bottom part of ECAL(iphi,ieta)
   ieta_global_offset = 0;
   ieta_signed_offset = -ECAL_IETA_MAX_EXT;
-  fillPFCandsAtECAL_with_EEproj( hEvt_EE_EndtracksPt[0], hEvt_EE_EndtracksQPt[0], hEvt_EE_muonsPt[0], hEvt_EE_muonsQPt[0], ieta_global_offset, ieta_signed_offset );
+  fillPFCandsAtECAL_with_EEproj( hEvt_EE_EndtracksPt[0], hEvt_EE_EndtracksQPt[0],
+				 hEvt_EE_EndtracksPt_PV[0], hEvt_EE_EndtracksQPt_PV[0],
+				 hEvt_EE_EndtracksPt_nPV[0], hEvt_EE_EndtracksQPt_nPV[0],
+				 hEvt_EE_muonsPt[0], hEvt_EE_muonsQPt[0], 
+				 hEvt_EE_muonsPt_PV[0], hEvt_EE_muonsQPt_PV[0], 
+				 ieta_global_offset, ieta_signed_offset );
 
   // Fill middle part of ECAL(iphi,ieta) with the EB rechits.
   ieta_global_offset = 55;
@@ -181,6 +285,7 @@ void RecHitAnalyzer::fillPFCandsAtECALstitched ( const edm::Event& iEvent, const
 
     trackPt_ = thisTrk->pt();
     trackQ_  = thisTrk->charge();
+    float trackz0_ =  ( !vtxs.empty() ? thisTrk->dz(vtxs[0].position()) : thisTrk->dz() );
 
     if ( std::abs(eta) > 3. ) continue;
     DetId id( spr::findDetIdECAL( caloGeom, eta, phi, false ) );
@@ -204,6 +309,22 @@ void RecHitAnalyzer::fillPFCandsAtECALstitched ( const edm::Event& iEvent, const
 	vECAL_muonsQPt_[idx_] += (trackPt_*trackQ_);
 	hECAL_muonsPt->Fill( iphi_, ieta_signed, trackPt_ );
       }
+
+      if(fabs(trackz0_) < z0PVCut_){
+	vECAL_EndtracksPt_PV_[idx_] += trackPt_;
+	vECAL_EndtracksQPt_PV_[idx_] += (trackPt_*trackQ_);
+
+	if(iPFC->particleId() == 3){
+	  vECAL_muonsPt_PV_[idx_] += trackPt_;
+	  vECAL_muonsQPt_PV_[idx_] += (trackPt_*trackQ_);
+	}
+
+      }else{
+	vECAL_EndtracksPt_nPV_[idx_] += trackPt_;
+	vECAL_EndtracksQPt_nPV_[idx_] += (trackPt_*trackQ_);
+      }
+
+
     }
 
   } // EB PFCands
@@ -212,6 +333,11 @@ void RecHitAnalyzer::fillPFCandsAtECALstitched ( const edm::Event& iEvent, const
   // Map EE+(phi,eta) to upper part of ECAL(iphi,ieta)
   ieta_global_offset = ECAL_IETA_MAX_EXT + EB_IETA_MAX;
   ieta_signed_offset = EB_IETA_MAX;
-  fillPFCandsAtECAL_with_EEproj( hEvt_EE_EndtracksPt[1], hEvt_EE_EndtracksQPt[1], hEvt_EE_muonsPt[1], hEvt_EE_muonsQPt[1], ieta_global_offset, ieta_signed_offset );
+  fillPFCandsAtECAL_with_EEproj( hEvt_EE_EndtracksPt[1], hEvt_EE_EndtracksQPt[1], 
+				 hEvt_EE_EndtracksPt_PV[1], hEvt_EE_EndtracksQPt_PV[1], 
+				 hEvt_EE_EndtracksPt_nPV[1], hEvt_EE_EndtracksQPt_nPV[1], 
+				 hEvt_EE_muonsPt[1], hEvt_EE_muonsQPt[1], 
+				 hEvt_EE_muonsPt_PV[1], hEvt_EE_muonsQPt_PV[1], 
+				 ieta_global_offset, ieta_signed_offset );
 
 } // fillPFCandsAtECALstitched()
