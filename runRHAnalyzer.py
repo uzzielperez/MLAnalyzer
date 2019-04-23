74f7ac090788579421e38794c86c449fe02f02b7
Uzziel <uzzie.perez@cern.ch>
2019-04-23 11:54:51 -0500

    Minor changes for hmass diphoton

--- runRHAnalyzer.py
+++ runRHAnalyzer.py
@@ -1,6 +1,7 @@
 import os
 
-cfg='RecHitAnalyzer/python/ConfFile_cfg.py'
+cmssw_base = os.getenv("CMSSW_BASE")
+cfg= cmssw_base + '/src/MLAnalyzer/RecHitAnalyzer/python/ConfFile_cfg.py'
 #inputFiles_='file:/eos/cms/store/user/mandrews/ML/FEVTDEBUG/h24gamma_1j_10K_100MeV_FEVTDEBUG_2016_25ns_Moriond17MC_PoissonOOTPU/180109_112606/0000/step_full_1.root'
 #inputFiles_='/store/user/mandrews/ML/AODSIM/T7WgStealth_800_200_AODSIM_noPU/180227_224514/0000/step_AODSIM_1.root'
 #inputFiles_='/store/user/mandrews/ML/AODSIM/WZToJets_TuneCUETP8M1_13TeV_pythia8_noPU_AODSIM/0/0000/step_full_1.root'
@@ -14,12 +15,19 @@ cfg='RecHitAnalyzer/python/ConfFile_cfg.py'
 
 #inputFiles_='/store/group/lpcml/mandrews/AODSIM/QCDToGG_Pt_80_120_13TeV_TuneCUETP8M1_noPU_AODSIM/180809_215549/0000/step_full_1.root'
 #inputFiles_='/store/group/lpcml/AODSIM/h24gamma_1j_1M_200MeV_2016_25ns_Moriond17MC_PoissonOOTPU_AODSIM/180907_083212/0000/step_full_1.root'
-inputFiles_='file:../step_AODSIM.root'
+#inputFiles_='file:../step_AODSIM.root'
 #inputFiles_='/store/group/lpcml/mandrews/AODSIM/SM2gammaj_1j_1M_2016_25ns_Moriond17MC_PoissonOOTPU_AODSIM/180906_154222/0000/step_full_98.root'
+
+#inputFiles_='/store/mc/RunIIFall17DRPremix/ADDGravToGG_NegInt-1_LambdaT-8000_M-2000To4000_TuneCP2_13TeV-pythia8/AODSIM/PU2017_94X_mc2017_realistic_v11-v1/40000/205BA2DE-1215-E911-A455-001E67DBE79B.root'
+
+#inputFiles_='/store/mc/RunIIFall17DRPremix/ADDGravToGG_NegInt-1_LambdaT-8000_M-2000To4000_TuneCP2_13TeV-pythia8/AODSIM/PU2017_94X_mc2017_realistic_v11-v1/40000/105A03BC-DB14-E911-A662-0025905C548A.root'
+
+inputFiles_='/store/mc/RunIIFall17MiniAODv2/ADDGravToGG_NegInt-1_LambdaT-8000_M-2000To4000_TuneCP2_13TeV-pythia8/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/40000/58C23153-2A1E-E911-955C-0CC47AFCC372.root'
+
+#inputFiles_='/store/mc/RunIIFall17DRPremix/ADDGravToGG_NegInt-1_LambdaT-8000_M-2000To4000_TuneCP2_13TeV-pythia8/AODSIM/PU2017_94X_mc2017_realistic_v11-v1/40000/*.root'
 
 maxEvents_=-1
 skipEvents_=0#
-
 
 cmd="cmsRun %s inputFiles=%s maxEvents=%d skipEvents=%d"%(cfg,inputFiles_,maxEvents_,skipEvents_)
 print '%s'%cmd
